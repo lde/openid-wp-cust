f7ffd24ec3420115ac960b48ec04bdc0c1136b43
lde <ldeloche@octo.com>
2012-03-20 12:25:59 +0100

    bug correction

--- openid/login.php
+++ openid/login.php
@@ -29,9 +29,9 @@ function openid_authenticate($user) {
 		}
 		}
 		
-	if((get_option('openid_allowed_regex') && !(preg_match(get_option('openid_allowed_regex'),$_POST['openid_identifier'])))||(get_option('openid_fobiden_regex_url') && (preg_match(get_option('openid_fobiden_regex_url'),$_POST['openid_identifier'])))){
+	if(((get_option('openid_allowed_regex') and (!(preg_match(get_option('openid_allowed_regex'),$_POST['openid_identifier'])))) or (get_option('openid_fobiden_regex_url') && (preg_match(get_option('openid_fobiden_regex_url'),$_POST['openid_identifier'])))) and $_POST['openid_identifier']){
 		die('You cannot login with this identity provider <a href="javascript:history.go(-1)">[Go Back]</a>');
-		}
+			}
 	
 		if ( array_key_exists('openid_identifier', $_POST) && $_POST['openid_identifier'] ) {
 			if (get_option('openid_regex_editor') && (preg_match(get_option('openid_regex_editor'),$_POST['openid_identifier']))){
@@ -89,7 +89,7 @@ function openid_finish_login($identity_url, $action) {
 	$user_id = get_user_by_openid($identity_url);
 	if ( $identity_url && !$user_id && get_option('users_can_register') ) {
 		if (get_option('openid_allowed_regex')){
-			if ((preg_match(get_option('openid_allowed_regex'),$_POST['openid_identifier']))){
+			if ((preg_match(get_option('openid_allowed_regex'),$identity_url))){
 			$user_data =& openid_get_user_data($identity_url);
 			openid_create_new_user($identity_url, $user_data);	
 			}
